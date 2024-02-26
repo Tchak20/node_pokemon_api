f66099c10ea3f2f5f76463211765600c0dab9ff0
Tchak20 <tchakourabastou@gmail.com>
2024-02-26 17:10:07 +0100

    Definition des routes avec express js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 // Point d'entrer de l'application node js qui peut être aussi index.js ou main.js
 const express = require('express')
+let pokemons = require('./mock-pokemon')
 
 const app = express()
 const port = 3000
 
+
 app.get('/', (req,res) => res.send('Hello, Express 2'))
+
+app.get('/api/pokemons/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send('Vous avez demandé le pokemon n '+pokemon.name)
+})
+
+app.get('/api/pokemons', (req,res) => {
+    res.send('Vous avez demandé le pokemon n '+pokemons.length)
+})
 
 app.listen(port, () => console.log('Notre application nodejs est démarrée sur : http://localhost:'+port))
