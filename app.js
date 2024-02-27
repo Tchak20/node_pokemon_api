d65850d69adb28f7e0bcf016ddb7898367f3e63d
Tchak20 <tchakourabastou@gmail.com>
2024-02-27 15:13:46 +0100

    middlewire

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 // Point d'entrer de l'application node js qui peut être aussi index.js ou main.js
 const express = require('express')
+const morgan = require('morgan')
+const favicon = require('serve-favicon')
 let pokemons = require('./mock-pokemon')
 
 const app = express()
 const port = 3000
 
+app
+//.use(favicon(__filename + '/favicon.ico'))
+.use(morgan('dev'))
 
 app.get('/', (req,res) => res.send('Hello, Express 2'))
 
 app.get('/api/pokemons/:id', (req,res) => {
     const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
-    res.send('Vous avez demandé le pokemon n '+pokemon.name)
+    res.json(pokemon)
 })
 
 app.get('/api/pokemons', (req,res) => {
